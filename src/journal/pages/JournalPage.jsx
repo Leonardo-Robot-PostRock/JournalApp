c2dbf9beb49fe476efc6931805bcf256c57dee75
Leonardo <leonardo38361@gmail.com>
2023-08-01 17:26:46 -0300

    envolviendo la app con layout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
 	return (
 		<>
-			<Typography variant='h1'>JournalPage</Typography>
+			<JournalLayout>
+				<Typography>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor,
+					nulla. Non voluptatem, quis expedita cum minima ex quasi unde
+					explicabo omnis. Quos earum repellat totam ut illo repudiandae
+					voluptas rerum! Quae reiciendis est eos sequi expedita consectetur
+					corporis quo! Sint amet odit ipsa impedit velit itaque nihil similique
+					non ab quia, distinctio totam quae. Distinctio eligendi veniam
+					praesentium a adipisci!
+				</Typography>
+			</JournalLayout>
 		</>
 	);
 };
