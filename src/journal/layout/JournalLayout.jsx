a963ecf6378657e0fee3a3e96549426750faf657
Leonardo <leonardo38361@gmail.com>
2023-08-01 19:24:27 -0300

    import Navbar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,12 +1,13 @@
 import { Box } from '@mui/material';
 import React from 'react';
-
+import { Navbar } from '../components/Navbar';
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
 	return (
-		<Box sx={{ display: "flex"}}>
+		<Box sx={{ display: 'flex' }}>
+			<Navbar drawerWidth={drawerWidth} />
 			<Box component='main' sx={{ flexGrow: 1, p: 3 }}>
 				{children}
 			</Box>
