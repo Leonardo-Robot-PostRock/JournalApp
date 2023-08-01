4d21afc461a38ceb642d8c786ccafb1f8c01d2a0
Leonardo <leonardo38361@gmail.com>
2023-08-01 00:13:32 -0300

    estilo de LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { Grid, TextField, Typography } from '@mui/material';
+import React from 'react';
 
 export const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+	return (
+		<Grid
+			container
+			spacing={0}
+			direction='column'
+			alignItems='center'
+			justifyContent='center'
+			sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+		>
+			<Grid
+				item
+				className='box-shadow'
+				xs={3}
+				sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
+			>
+				<Typography variant='h5' sx={{ mb: 1 }}>
+					Login
+				</Typography>
+
+				<form>
+					<Grid container>
+						<Grid item xs={12} sx={{mt:2}}>
+							<TextField
+								label='Correo'
+								type='email'
+								placeholder='correo@gmail.com'
+                fullWidth
+							></TextField>
+						</Grid>
+
+            <Grid item xs={12} sx={{mt:2}}>
+              <TextField label="Contraseña" type='password' placeholder='Contraseña' fullWidth>
+
+              </TextField>
+            </Grid>
+					</Grid>
+				</form>
+			</Grid>
+		</Grid>
+	);
+};
