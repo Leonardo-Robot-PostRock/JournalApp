bfedcff6b0650acd951ec4580573f67cb84019b0
Leonardo <leonardo38361@gmail.com>
2023-08-02 00:52:29 -0300

    importacion sidebar y toolbar de mui

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,6 +1,6 @@
-import { Box } from '@mui/material';
+import { Box, Toolbar } from '@mui/material';
 import React from 'react';
-import { Navbar } from '../components/Navbar';
+import { Navbar, SideBar } from '../components';
 
 const drawerWidth = 240;
 
@@ -8,7 +8,9 @@ export const JournalLayout = ({ children }) => {
 	return (
 		<Box sx={{ display: 'flex' }}>
 			<Navbar drawerWidth={drawerWidth} />
+			<SideBar drawerWidth={drawerWidth} />
 			<Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+				<Toolbar />
 				{children}
 			</Box>
 		</Box>
