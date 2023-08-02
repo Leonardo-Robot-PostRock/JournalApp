b01a22aacd4a4c4a61ce74446a2c49fc491c3385
Leonardo <leonardo38361@gmail.com>
2023-08-02 03:27:47 -0300

    se sube el estado al componente padre

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import styled from '@emotion/styled';
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+	({ theme, open }) => ({
+		flexGrow: 1,
+		padding: theme.spacing(3),
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+		marginLeft: `-${drawerWidth}px`,
+		...(open && {
+			transition: theme.transitions.create('margin', {
+				easing: theme.transitions.easing.easeOut,
+				duration: theme.transitions.duration.enteringScreen,
+			}),
+			marginLeft: 0,
+		}),
+	})
+);
 
 export const JournalPage = () => {
+	const [open, setOpen] = useState(false);
+
 	return (
 		<>
-			<JournalLayout>
-				<Typography>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor,
-					nulla. Non voluptatem, quis expedita cum minima ex quasi unde
-					explicabo omnis. Quos earum repellat totam ut illo repudiandae
-					voluptas rerum! Quae reiciendis est eos sequi expedita consectetur
-					corporis quo! Sint amet odit ipsa impedit velit itaque nihil similique
-					non ab quia, distinctio totam quae. Distinctio eligendi veniam
-					praesentium a adipisci!
-				</Typography>
-			</JournalLayout>
+			<Main open={open}>
+				<JournalLayout open={open} setOpen={setOpen}>
+					<Typography paragraph>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor,
+						nulla. Non voluptatem, quis expedita cum minima ex quasi unde
+						explicabo omnis. Quos earum repellat totam ut illo repudiandae
+						voluptas rerum! Quae reiciendis est eos sequi expedita consectetur
+						corporis quo! Sint amet odit ipsa impedit velit itaque nihil
+						similique non ab quia, distinctio totam quae. Distinctio eligendi
+						veniam praesentium a adipisci!
+					</Typography>
+				</JournalLayout>
+			</Main>
 		</>
 	);
 };
