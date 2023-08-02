853630dcc4feabaebb5b71e41eb916276f27cd06
Leonardo <leonardo38361@gmail.com>
2023-08-02 03:27:16 -0300

    se envian props a componentes hihos

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,14 +1,14 @@
 import { Box, Toolbar } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, SideBar } from '../components';
 
 const drawerWidth = 240;
 
-export const JournalLayout = ({ children }) => {
+export const JournalLayout = ({ children, open, setOpen }) => {
 	return (
 		<Box sx={{ display: 'flex' }}>
-			<Navbar drawerWidth={drawerWidth} />
-			<SideBar drawerWidth={drawerWidth} />
+			<Navbar drawerWidth={drawerWidth} open={open} setOpen={setOpen} />
+			<SideBar drawerWidth={drawerWidth} open={open} setOpen={setOpen} />
 			<Box component='main' sx={{ flexGrow: 1, p: 3 }}>
 				<Toolbar />
 				{children}
