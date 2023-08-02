4def8fc853f0325ff0e565ec08dbb298d2fc8e24
Leonardo <leonardo38361@gmail.com>
2023-08-02 03:26:33 -0300

    se implementa sidebar dinámico

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,9 +1,16 @@
+import React from 'react';
+
 import { TurnedInNot } from '@mui/icons-material';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { styled, useTheme } from '@mui/material/styles';
+
 import {
 	Box,
 	Divider,
 	Drawer,
 	Grid,
+	IconButton,
 	List,
 	ListItem,
 	ListItemButton,
@@ -12,22 +19,52 @@ import {
 	Toolbar,
 	Typography,
 } from '@mui/material';
-import React from 'react';
 
-export const SideBar = ({ drawerWidth }) => {
+const DrawerHeader = styled('div')(({ theme }) => ({
+	display: 'flex',
+	alignItems: 'center',
+	padding: theme.spacing(0, 1),
+	// necessary for content to be below app bar
+	...theme.mixins.toolbar,
+	justifyContent: 'flex-end',
+}));
+
+export const SideBar = ({ drawerWidth, open, setOpen }) => {
+	const theme = useTheme();
+
+	const handleDrawerClose = () => {
+		setOpen(false);
+		console.log('cerrando');
+	};
+
 	return (
 		<Box
 			component='nav'
 			sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
 		>
 			<Drawer
-				variant='permanent'
-				open
+				anchor='left'
+				variant='persistent'
+				open={open}
 				sx={{
-					display: { xs: 'block' },
-					'& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-				}}
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                      width: drawerWidth,
+                      boxSizing: 'border-box',
+                    },
+                  }}
 			>
+				<DrawerHeader>
+					<IconButton onClick={handleDrawerClose}>
+						{theme.direction === 'ltr' ? (
+							<ChevronLeftIcon />
+						) : (
+							<ChevronRightIcon />
+						)}
+					</IconButton>
+				</DrawerHeader>
+				<Divider />
 				<Toolbar>
 					<Typography variant='h6' noWrap component='div'>
 						Leonardo Puebla
@@ -42,10 +79,14 @@ export const SideBar = ({ drawerWidth }) => {
 								<ListItemIcon>
 									<TurnedInNot />
 								</ListItemIcon>
-                                <Grid container>
-                                    <ListItemText primary={text}/>
-                                    <ListItemText secondary={'Fugiat adipisicing exercitation ullamco nostrud.'}/>
-                                </Grid>
+								<Grid container>
+									<ListItemText primary={text} />
+									<ListItemText
+										secondary={
+											'Fugiat adipisicing exercitation ullamco nostrud.'
+										}
+									/>
+								</Grid>
 							</ListItemButton>
 						</ListItem>
 					))}
