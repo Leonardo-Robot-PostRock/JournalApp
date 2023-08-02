635b532dbdf73cb8ab860d1739ee6ad6b3e2e7d7
Leonardo <leonardo38361@gmail.com>
2023-08-02 03:25:41 -0300

    Navbar dinamico

--- src/journal/components/Navbar.jsx
+++ src/journal/components/Navbar.jsx
@@ -1,21 +1,42 @@
-import { LogoutOutlined, MenuOutlined } from '@mui/icons-material';
-import { AppBar, Grid, IconButton, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 
-export const Navbar = ({ drawerWidth = 240 }) => {
+import { LogoutOutlined, MenuOutlined } from '@mui/icons-material';
+import { Grid, IconButton, Toolbar, Typography, styled } from '@mui/material';
+import MuiAppBar from '@mui/material/AppBar';
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+	shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+	transition: theme.transitions.create(['margin', 'width'], {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	...(open && {
+		width: `calc(100% - ${drawerWidth}px)`,
+		marginLeft: `${drawerWidth}px`,
+		transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	}),
+}));
+
+export const Navbar = ({ open, setOpen }) => {
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+
 	return (
-		<AppBar
-			position='fixed'
-			sx={{
-				width: { sm: `calc(100% - ${drawerWidth}px)` },
-				ml: { sm: `${drawerWidth}px` },
-			}}
-		>
+		<AppBar position='fixed' open={open}>
 			<Toolbar>
 				<IconButton
 					color='inherit'
+					aria-label='open drawer'
+					onClick={handleDrawerOpen}
 					edge='start'
-					sx={{ mr: 2, display: { sm: 'none' } }}
+					sx={{ mr: 2, ...(open && { display: 'none' }) }}
 				>
 					<MenuOutlined />
 				</IconButton>
