3ec36a2160c171e25e700f1df86096ab78ce6daf
Leonardo <leonardo38361@gmail.com>
2023-08-02 21:34:38 -0300

    importacion de componente

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -4,6 +4,7 @@ import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
 
 import { Main } from '../styled-components/Main';
+import { NothingSelectedView } from '../views/NothingSelectedView';
 
 export const JournalPage = () => {
 	const [open, setOpen] = useState(false);
@@ -12,15 +13,7 @@ export const JournalPage = () => {
 		<>
 			<Main open={open}>
 				<JournalLayout open={open} setOpen={setOpen}>
-					<Typography paragraph>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor,
-						nulla. Non voluptatem, quis expedita cum minima ex quasi unde
-						explicabo omnis. Quos earum repellat totam ut illo repudiandae
-						voluptas rerum! Quae reiciendis est eos sequi expedita consectetur
-						corporis quo! Sint amet odit ipsa impedit velit itaque nihil
-						similique non ab quia, distinctio totam quae. Distinctio eligendi
-						veniam praesentium a adipisci!
-					</Typography>
+					<NothingSelectedView />
 				</JournalLayout>
 			</Main>
 		</>
