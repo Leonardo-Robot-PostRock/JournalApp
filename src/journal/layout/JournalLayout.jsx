fc0905d7d91aad143376eb0489ae4b3f587bb36e
Leonardo <leonardo38361@gmail.com>
2023-08-03 03:05:12 -0300

    componente cascaron recibe nuevo parametro

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,15 +1,36 @@
 import { Box, Toolbar } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, SideBar } from '../components';
 
 const drawerWidth = 240;
 
-export const JournalLayout = ({ children, open, setOpen }) => {
+export const JournalLayout = ({ children, container }) => {
+	const [mobileOpen, setMobileOpen] = useState(false);
+
+	const handleDrawerToggle = () => {
+		setMobileOpen(!mobileOpen);
+	};
+
 	return (
 		<Box sx={{ display: 'flex' }}>
-			<Navbar drawerWidth={drawerWidth} open={open} setOpen={setOpen} />
-			<SideBar drawerWidth={drawerWidth} open={open} setOpen={setOpen} />
-			<Box component='main' sx={{ flexGrow: 1}}>
+			<Navbar
+				drawerWidth={drawerWidth}
+				handleDrawerToggle={handleDrawerToggle}
+			/>
+			<SideBar
+				drawerWidth={drawerWidth}
+				container={container}
+				mobileOpen={mobileOpen}
+				handleDrawerToggle={handleDrawerToggle}
+			/>
+			<Box
+				component='main'
+				sx={{
+					flexGrow: 1,
+					p: 3,
+					width: { sm: `calc(100% - ${drawerWidth}px)` },
+				}}
+			>
 				<Toolbar />
 				{children}
 			</Box>
