12711c16a466ccda829afe6f9b2f6483e6223f8c
Leonardo <leonardo38361@gmail.com>
2023-08-03 03:00:53 -0300

    Se cambia el componente

--- src/journal/components/Navbar.jsx
+++ src/journal/components/Navbar.jsx
@@ -1,43 +1,54 @@
 import React from 'react';
 
 import { LogoutOutlined, MenuOutlined } from '@mui/icons-material';
-import { Grid, IconButton, Toolbar, Typography } from '@mui/material';
+import {
+	AppBar,
+	Box,
+	CssBaseline,
+	Grid,
+	IconButton,
+	Toolbar,
+	Typography,
+} from '@mui/material';
 
-import { AppBar } from '../styled-components/AppBar';
-
-export const Navbar = ({ open, setOpen }) => {
-	const handleDrawerOpen = () => {
-		setOpen(true);
-	};
-
+export const Navbar = ({ drawerWidth, handleDrawerToggle }) => {
 	return (
-		<AppBar position='fixed' open={open}>
-			<Toolbar>
-				<IconButton
-					color='inherit'
-					aria-label='open drawer'
-					onClick={handleDrawerOpen}
-					edge='start'
-					sx={{ mr: 2, ...(open && { display: 'none' }) }}
-				>
-					<MenuOutlined />
-				</IconButton>
-
-				<Grid
-					container
-					direction='row'
-                    flexWrap="nowrap"
-                    justifyContent='space-between'
-					alignItems='center'
-				>
-					<Typography variant='h6' noWrap flexShrink={0} component='div'>
-						JournalApp
-					</Typography>
-					<IconButton color='error'>
-						<LogoutOutlined />
+		<Box sx={{ display: 'flex' }}>
+			<CssBaseline />
+			<AppBar
+				position='fixed'
+				sx={{
+					width: { sm: `calc(100% - ${drawerWidth}px)` },
+					ml: { sm: `${drawerWidth}px` },
+				}}
+			>
+				<Toolbar>
+					<IconButton
+						color='inherit'
+						aria-label='open drawer'
+						edge='start'
+						onClick={handleDrawerToggle}
+						sx={{ mr: 2, display: { sm: 'none' } }}
+					>
+						<MenuOutlined />
 					</IconButton>
-				</Grid>
-			</Toolbar>
-		</AppBar>
+
+					<Grid
+						container
+						direction='row'
+						flexWrap='nowrap'
+						justifyContent='space-between'
+						alignItems='center'
+					>
+						<Typography variant='h6' noWrap flexShrink={0} component='div'>
+							JournalApp
+						</Typography>
+						<IconButton color='error'>
+							<LogoutOutlined />
+						</IconButton>
+					</Grid>
+				</Toolbar>
+			</AppBar>
+		</Box>
 	);
 };
