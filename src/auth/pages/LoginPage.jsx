2b4fd20f4ba6f8fc8ea6319a202719542deb92e2
Leonardo <leonardo38361@gmail.com>
2023-08-10 01:48:19 -0300

    uso del hook useForm, dispatch de funciones asyncronas, en evento onSubmit

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
 
 import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+
 import { AuthLayout } from '../Layout/AuthLayout';
+import { useForm } from '../../hooks';
+import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
 
 export const LoginPage = () => {
+	const dispatch = useDispatch();
+
+	const { email, password, onInputChange } = useForm({
+		email: 'leonardo@google.com',
+		password: '12345',
+	});
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+		dispatch(checkingAuthentication());
+	};
+
+	const onGoogleSignIn = () => {
+		dispatch(startGoogleSignIn());
+	};
+
 	return (
 		<AuthLayout title='Login'>
-			<form>
+			<form onSubmit={onSubmit}>
 				<Grid container>
 					<Grid item xs={12} sx={{ mt: 2 }}>
 						<TextField
@@ -16,6 +36,9 @@ export const LoginPage = () => {
 							type='email'
 							placeholder='correo@gmail.com'
 							fullWidth
+							name='email'
+							value={email}
+							onChange={onInputChange}
 						></TextField>
 					</Grid>
 
@@ -25,17 +48,25 @@ export const LoginPage = () => {
 							type='password'
 							placeholder='Contraseña'
 							fullWidth
+							name='password'
+							value={password}
+							onChange={onInputChange}
 						></TextField>
 					</Grid>
 
 					<Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
 						<Grid item xs={12} sm={6}>
-							<Button variant='contained' fullWidth>
+							<Button
+								type='submit'
+								onClick={onSubmit}
+								variant='contained'
+								fullWidth
+							>
 								Login
 							</Button>
 						</Grid>
 						<Grid item xs={12} sm={6}>
-							<Button variant='contained' fullWidth>
+							<Button variant='contained' fullWidth onClick={onGoogleSignIn}>
 								<Google />
 								<Typography sx={{ ml: 1 }}>Google</Typography>
 							</Button>
