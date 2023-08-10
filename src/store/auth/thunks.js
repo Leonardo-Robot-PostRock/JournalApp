866fca10f5af03f03d8c65bf56b32aeddaa874a7
Leonardo <leonardo38361@gmail.com>
2023-08-10 01:57:06 -0300

    comentado

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,6 +1,7 @@
 import { checkingCredentials } from './authSlice';
 
 export const checkingAuthentication = () => {
+	// Podría suprimirse esta funcion y enviar sólo la acción del reducer dado que es una tarea síncrona la que se ejecuta
 	return async (dispatch) => {
 		dispatch(checkingCredentials());
 	};
