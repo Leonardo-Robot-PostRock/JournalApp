093042257f39386d0c546fca4a6a77bb9444b90e
Leonardo <leonardo38361@gmail.com>
2023-08-11 01:30:37 -0300

    se llama la función asincrona para logearse con google

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { signInWithGoogle } from '../../firebase/providers';
 import { checkingCredentials } from './authSlice';
 
 export const checkingAuthentication = () => {
@@ -10,5 +11,7 @@ export const checkingAuthentication = () => {
 export const startGoogleSignIn = () => {
 	return async (dispatch) => {
 		dispatch(checkingCredentials());
+		const result = await signInWithGoogle();
+		console.log({ result });
 	};
 };
