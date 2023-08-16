7ce23090420ea2bdbf9e6be82252f0750d0355f2
Leonardo <leonardo38361@gmail.com>
2023-08-16 16:48:07 -0300

    uso del useForm en Register page

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -3,11 +3,26 @@ import { Link as RouterLink } from 'react-router-dom';
 
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../Layout/AuthLayout';
+import { useForm } from '../../hooks';
+
+const formData = {
+	email: 'leonardo@google.com',
+	password: '123456',
+	displayName: 'Leonardo Puebla',
+};
 
 export const RegisterPage = () => {
+	const { displayName, email, password, onInputChange, formState } =
+		useForm(formData);
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		console.log(formState);
+	};
+
 	return (
 		<AuthLayout title='Crear cuenta'>
-			<form>
+			<form onSubmit={onSubmit}>
 				<Grid container>
 					<Grid item xs={12} sx={{ mt: 2 }}>
 						<TextField
@@ -15,6 +30,9 @@ export const RegisterPage = () => {
 							type='text'
 							placeholder='Nombre completo'
 							fullWidth
+							name='displayName'
+							value={displayName}
+							onChange={onInputChange}
 						></TextField>
 					</Grid>
 					<Grid item xs={12} sx={{ mt: 2 }}>
@@ -23,6 +41,9 @@ export const RegisterPage = () => {
 							type='email'
 							placeholder='Correo'
 							fullWidth
+							name='email'
+							value={email}
+							onChange={onInputChange}
 						></TextField>
 					</Grid>
 
@@ -32,18 +53,21 @@ export const RegisterPage = () => {
 							type='password'
 							placeholder='Contraseña'
 							fullWidth
+							name='password'
+							value={password}
+							onChange={onInputChange}
 						></TextField>
 					</Grid>
 
 					<Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
 						<Grid item xs={12} sm={6}>
-							<Button variant='contained' fullWidth>
+							<Button variant='contained' type='submit' fullWidth>
 								Crear cuenta
 							</Button>
 						</Grid>
 					</Grid>
 					<Grid container direction='row' justifyContent='end'>
-						<Typography sx={{mr:1}}>¿Ya tienes cuenta?</Typography>
+						<Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
 						<Link component={RouterLink} color='inherit' to='/auth/login'>
 							Ingresar
 						</Link>
