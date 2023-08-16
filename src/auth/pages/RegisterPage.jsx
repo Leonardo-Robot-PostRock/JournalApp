735cf93a38f91157709b4feb72a5715e968b90af
Leonardo <leonardo38361@gmail.com>
2023-08-16 17:00:27 -0300

    creacion de funcion formValidationsque devuelve objeto, manejo de errores

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -11,9 +11,27 @@ const formData = {
 	displayName: 'Leonardo Puebla',
 };
 
+const formValidations = () => {
+	email: [(value) => value.includes('@'), 'El correo debe tener un @'];
+	password: [
+		(value) => value.length >= 6,
+		'El password debe de tener más de 6 letras',
+	];
+	displayName: [(value) => value.length >= 1, 'El nombre debe ser obligatorio'];
+};
+
 export const RegisterPage = () => {
-	const { displayName, email, password, onInputChange, formState } =
-		useForm(formData);
+	const {
+		displayName,
+		email,
+		password,
+		onInputChange,
+		formState,
+		isFormValid,
+		emailValid,
+		displayNameValid,
+		passwordValid,
+	} = useForm(formData, formValidations);
 
 	const onSubmit = (event) => {
 		event.preventDefault();
@@ -33,6 +51,8 @@ export const RegisterPage = () => {
 							name='displayName'
 							value={displayName}
 							onChange={onInputChange}
+							error={!displayName}
+							helperText={displayName}
 						></TextField>
 					</Grid>
 					<Grid item xs={12} sx={{ mt: 2 }}>
@@ -44,6 +64,8 @@ export const RegisterPage = () => {
 							name='email'
 							value={email}
 							onChange={onInputChange}
+							error={false}
+							helperText='El nombre es obligatorio'
 						></TextField>
 					</Grid>
 
