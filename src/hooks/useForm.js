d0ea1aa88bf53ec07f9c28978e4f2e508005abe4
Leonardo <leonardo38361@gmail.com>
2023-08-17 16:48:28 -0300

    useForm recibe nuevo parametro, se crea funcion createValidators, valida cada campo de que sea válido

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useForm = (initialForm = {}, formValidations = {}) => {
 	const [formState, setFormState] = useState(initialForm);
+	const [formValidation, setFormValidation] = useState({});
+
+	useEffect(() => {
+		createValidators();
+	}, [formState]);
 
 	const onInputChange = ({ target }) => {
 		const { name, value } = target;
@@ -15,10 +20,31 @@ export const useForm = (initialForm = {}, formValidations = {}) => {
 		setFormState(initialForm);
 	};
 
+	const createValidators = () => {
+		const formCheckedValues = {};
+
+		// Obtiene la función de validación y el mensaje de error correspondiente del objeto formValidations para el campo actual
+		for (const formField of Object.keys(formValidations)) {
+			console.log(formField);
+			const [fn, errorMessage] = formValidations[formField];
+
+			// Evalúa la función de validación (fn) en el valor actual del campo en formState
+			// Si la función devuelve verdadero, el campo se considera válido y se asigna "null" a la propiedad `${formField}Valid` en formCheckedValues.
+			// Si la función devuelve falso, el campo se considera inválido y se asigna el mensaje de error a la propiedad `${formField}Valid` en formCheckedValues.
+			formCheckedValues[`${formField}Valid`] = fn(formState[formField])
+				? null
+				: errorMessage;
+
+			// Actualiza el estado de validación del formulario con las últimas validaciones realizadas
+			setFormValidation(formCheckedValues);
+		}
+	};
+
 	return {
 		...formState,
 		formState,
 		onInputChange,
 		onResetForm,
+		...formValidation,
 	};
 };
