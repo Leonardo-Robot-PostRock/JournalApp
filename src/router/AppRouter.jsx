707fed2d66fef4b5e2b9eb181144bd648ff156d7
Leonardo <leonardo38361@gmail.com>
2023-08-24 00:55:44 -0300

    se limpia el AppRouter con un custom hook

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,26 +1,11 @@
-import React, { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/pages';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
-import { useDispatch, useSelector } from 'react-redux';
+import { useCheckAuth } from '../hooks';
 import { CheckingAuth } from '../ui';
-import { onAuthStateChanged } from 'firebase/auth';
-import { FirebaseAuth } from '../firebase/config';
-import { login, logout } from '../store/auth';
 
 export const AppRouter = () => {
-	const { status } = useSelector((state) => state.auth);
-	const dispatch = useDispatch();
-
-	useEffect(() => {
-		onAuthStateChanged(FirebaseAuth, async (user) => {
-			if (!user) return dispatch(logout());
-
-			const { uid, email, displayName, photoURL } = user;
-
-			dispatch(login({ uid, email, displayName, photoURL }));
-		});
-	}, []);
+	const status  = useCheckAuth();
 
 	if (status === 'checking') {
 		return <CheckingAuth />;
