8fedc261a82424c699eb66b1d984f00b3e708622
Leonardo <leonardo38361@gmail.com>
2023-08-23 23:04:12 -0300

    se extrae status del store y si el status es checking se muestra el loading

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/pages';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
+import { useSelector } from 'react-redux';
+import { CheckingAuth } from '../ui';
 
 export const AppRouter = () => {
+	const { status } = useSelector((state) => state.auth);
+
+	if (status === 'checking') {
+		return <CheckingAuth />;
+	}
+
 	return (
 		<Routes>
 			{/* Login y Registro */}
