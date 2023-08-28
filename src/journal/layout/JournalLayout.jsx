d9ace0b03035dee68b664d21a7b4f301658cb022
Leonardo <leonardo38361@gmail.com>
2023-08-28 19:46:50 -0300

    se extrae drawerWidth de custom hook

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import { Box, Toolbar } from '@mui/material';
 
 import { Navbar, SideBar } from '../components';
+import useResponsiveDrawerWidth from '../../hooks';
 
-const drawerWidth = 240;
+// const drawerWidth = 240;
 
 export const JournalLayout = ({ children, container }) => {
 	const [mobileOpen, setMobileOpen] = useState(false);
+	const drawerWidth = useResponsiveDrawerWidth(); // Use the custom hook to get the dynamic width
 
 	const handleDrawerToggle = () => {
 		setMobileOpen(!mobileOpen);
@@ -15,12 +17,7 @@ export const JournalLayout = ({ children, container }) => {
 	return (
 		<Box sx={{ display: 'flex' }} className='animate__animated animate__fadeIn animate__faster'>
 			<Navbar drawerWidth={drawerWidth} handleDrawerToggle={handleDrawerToggle} />
-			<SideBar
-				drawerWidth={drawerWidth}
-				container={container}
-				mobileOpen={mobileOpen}
-				handleDrawerToggle={handleDrawerToggle}
-			/>
+			<SideBar drawerWidth={drawerWidth} container={container} mobileOpen={mobileOpen} handleDrawerToggle={handleDrawerToggle} />
 			<Box
 				component='main'
 				sx={{
