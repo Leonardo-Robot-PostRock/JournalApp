5e151253033726ab62fbdb1a5db4c92d6cd5f73c
Leonardo <leonardo38361@gmail.com>
2023-08-28 17:01:09 -0300

    se crea startLoadingNotes thunks, se envia uid a loadNotes, se destructura notes y se envia las notes a setNotes

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -2,7 +2,8 @@ import { collection, doc, setDoc } from 'firebase/firestore/lite';
 
 import { FirebaseDB } from '../../firebase/config';
 
-import { addNewEmptyNote, savingNewNote, setActiveNote } from './journalSlice';
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from './journalSlice';
+import { loadNotes } from '../../helpers';
 
 export const startNewNote = () => {
 	return async (dispatch, getState) => {
@@ -17,11 +18,23 @@ export const startNewNote = () => {
 
 		const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
 
-		const setDocResp = await setDoc(newDoc, newNote);
+		await setDoc(newDoc, newNote);
 
 		newNote.id = newDoc.id;
 
 		dispatch(addNewEmptyNote(newNote));
 		dispatch(setActiveNote(newNote));
+	};
+};
+
+export const startLoadingNotes = () => {
+	return async (dispatch, getState) => {
+		const { uid } = getState().auth;
+
+		if (!uid) throw new Error('El UID del usuario no existe');
+
+		const notes = await loadNotes(uid);
+
+		dispatch(setNotes(notes));
 	};
 };
