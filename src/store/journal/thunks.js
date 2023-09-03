ce21e5313f305e4877464f599789773e4e4728a2
Leonardo <leonardo38361@gmail.com>
2023-09-03 01:24:32 -0300

    se actualiza la nota con dispatch

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -8,6 +8,7 @@ import {
 	setActiveNote,
 	setNotes,
 	setSaving,
+	updateNote,
 } from './journalSlice';
 import { loadNotes } from '../../helpers';
 
@@ -48,14 +49,16 @@ export const startLoadingNotes = () => {
 export const startSaveNote = () => {
 	return async (dispatch, getState) => {
 		dispatch(setSaving());
+
 		const { uid } = getState().auth;
 		const { active: note } = getState().journal;
 
 		const noteToFireStore = { ...note };
-
 		delete noteToFireStore.id;
-		const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`);
 
+		const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`);
 		await setDoc(docRef, noteToFireStore, { merge: true });
+
+		dispatch(updateNote(note));
 	};
 };
