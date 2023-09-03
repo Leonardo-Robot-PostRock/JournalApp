794cf234c4959c5d3ad7cdbd430efe6bdcba7bd8
Leonardo <leonardo38361@gmail.com>
2023-09-03 01:12:52 -0300

    dispatch de setSaving. Se borra id para evitar duplicación de informacion. Se captura el path, se actualiza info en FireStore

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -2,7 +2,13 @@ import { collection, doc, setDoc } from 'firebase/firestore/lite';
 
 import { FirebaseDB } from '../../firebase/config';
 
-import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from './journalSlice';
+import {
+	addNewEmptyNote,
+	savingNewNote,
+	setActiveNote,
+	setNotes,
+	setSaving,
+} from './journalSlice';
 import { loadNotes } from '../../helpers';
 
 export const startNewNote = () => {
@@ -36,5 +42,20 @@ export const startLoadingNotes = () => {
 		const notes = await loadNotes(uid);
 
 		dispatch(setNotes(notes));
+	};
+};
+
+export const startSaveNote = () => {
+	return async (dispatch, getState) => {
+		dispatch(setSaving());
+		const { uid } = getState().auth;
+		const { active: note } = getState().journal;
+
+		const noteToFireStore = { ...note };
+
+		delete noteToFireStore.id;
+		const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`);
+
+		await setDoc(docRef, noteToFireStore, { merge: true });
 	};
 };
