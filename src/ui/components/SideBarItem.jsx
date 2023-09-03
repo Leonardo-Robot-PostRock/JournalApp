95c790abe41ec3cd4396a07a0072ee35ffa59de0
Leonardo <leonardo38361@gmail.com>
2023-09-02 21:58:41 -0300

    se actica la nota

--- src/ui/components/SideBarItem.jsx
+++ src/ui/components/SideBarItem.jsx
@@ -1,16 +1,27 @@
 import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { TurnedInNot } from '@mui/icons-material';
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 
-export const SideBarItem = ({ title, body, id }) => {
+import { setActiveNote } from '../../store/journal/journalSlice';
+
+export const SideBarItem = ({ title, body, date, id, imageUrls = [] }) => {
+	const { active } = useSelector((state) => state.journal);
+	const dispatch = useDispatch();
+
+	const onActiveNote = () => {
+		console.log(active);
+		dispatch(setActiveNote({ title, body, date, id, imageUrls }));
+	};
+
 	const newTitle = useMemo(() => {
-		return title.length > 17 ? title.substring(0,17) + '...' : title;
+		return title.length > 17 ? title.substring(0, 17) + '...' : title;
 	}, [title]);
 
 	return (
 		<ListItem disablePadding>
-			<ListItemButton>
+			<ListItemButton onClick={onActiveNote}>
 				<ListItemIcon>
 					<TurnedInNot />
 				</ListItemIcon>
