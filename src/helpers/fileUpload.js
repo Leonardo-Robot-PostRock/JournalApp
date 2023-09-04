a50dab1f5b744a664da6fe3fa3d8c365d15afd60
Leonardo <leonardo38361@gmail.com>
2023-09-04 16:18:48 -0300

    se realiza petición a cloudinary, método POST para subir imagen

---
+++ src/helpers/fileUpload.js
@@ -0,0 +1,26 @@
+export const fileUpload = async (file) => {
+	if (!file) throw new Errror('No tenemos ningún archivo a subir');
+	const cloudUrl = 'https://api.cloudinary.com/v1_1/colection/upload';
+
+	//Body de la petición https, también visto en POSTMAN
+	const formData = new FormData();
+	formData.append('upload_preset', 'journal');
+	formData.append('file', file);
+
+	try {
+		const resp = await fetch(cloudUrl, {
+			method: 'POST',
+			body: formData,
+		});
+		console.log(resp);
+
+		if (!resp.ok) throw new Error('No se pudo subir imagen');
+		const cloudResp = await resp.json();
+		console.log({ cloudResp });
+
+		return cloudResp.secure_url;
+	} catch (error) {
+		console.log(error);
+		throw new Error(error);
+	}
+};
