e93e90e9f29744b15e12b95aa24bbfbceeb7ba86
Leonardo <leonardo38361@gmail.com>
2024-07-20 05:06:21 -0300

    delete images design

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -1,18 +1,28 @@
-import { ImageList, ImageListItem } from '@mui/material';
+import { Button, ImageList, ImageListItem } from '@mui/material';
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
 
 export const ImageGallery = ({ images }) => {
-	return (
-		<ImageList sx={{ width: '100%', height: 'auto' }} cols={4} rowHeight='auto'>
-			{images.map((images) => (
-				<ImageListItem key={images}>
-					<img
-						src={`${images}?w=164&h=164&fit=crop&auto=format`}
-						srcSet={`${images}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-						alt='Imagen de la nota'
-						loading='lazy'
-					/>
-				</ImageListItem>
-			))}
-		</ImageList>
-	);
+  return (
+    <ImageList sx={{ width: '100%', height: 'auto' }} cols={2} rowHeight="auto">
+      {images.map((images) => (
+        <ImageListItem key={images}>
+          <img
+            src={`${images}?w=164&h=164&fit=crop&auto=format`}
+            srcSet={`${images}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            alt="Imagen de la nota"
+            loading="lazy"
+          />
+          <Button
+            sx={{
+              position: 'absolute',
+              top: '4px',
+              color: 'red',
+            }}
+          >
+            <DeleteForeverRoundedIcon fontSize="large" />
+          </Button>
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
 };
